673af2e32e26daf4df803d7c11941b735116fc67
xdoctorwhoz <xavier.rodriguezalvarez@gmail.com>
2022-05-20 22:08:18 +0200

    work on power supply card design

--- components/cards/interfaces/power_supply.js
+++ components/cards/interfaces/power_supply.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from 'react'
 
 //
 import Link from 'next/link'
+import Image from 'next/image'
 
 //
 import {
@@ -120,28 +121,37 @@ function SliderAmps(props) {
  */
 function ButtonOnOff(props) {
     return (
-        <Button
-            // onClick={() => {
-            //     // console.log("click change !!", itrf.value)
-            //     let toggle_enable = false
-            //     if (itrf.enable === false) {
-            //         toggle_enable = true
-            //     }
-            //     itrf.co.client.publish(
-            //         itrf.base_topic + "/cmds/enable/set",
-            //         JSON.stringify({ enable: toggle_enable })
-            //     )
-            // }}
-            // sx={{
-            //     display: 'flex',
-            //     justifyContent: 'center',
-            //     flexDirection: 'column',
-            //     padding: 1
-            // }}
-        >
-            on
-            {/* {itrf.enable ? 'ON' : 'OFF'} */}
-        </Button>
+
+        <Box sx={{ display: 'flex', flexDirection: 'row', mb: '32px' }}>
+
+            {/* <Chip label="ON" color="success" /> */}
+            <Chip label="OFF" color="error" />
+
+            <Button
+                // onClick={() => {
+                //     // console.log("click change !!", itrf.value)
+                //     let toggle_enable = false
+                //     if (itrf.enable === false) {
+                //         toggle_enable = true
+                //     }
+                //     itrf.co.client.publish(
+                //         itrf.base_topic + "/cmds/enable/set",
+                //         JSON.stringify({ enable: toggle_enable })
+                //     )
+                // }}
+                // sx={{
+                //     display: 'flex',
+                //     justifyContent: 'center',
+                //     flexDirection: 'column',
+                //     padding: 1
+                // }}
+                color="success"
+            >
+                Turn ON
+                {/* {itrf.enable ? 'ON' : 'OFF'} */}
+            </Button>
+
+        </Box>
     )
 }
 
@@ -158,18 +168,45 @@ export default function CardPowerSupply(props) {
         <Card sx={{ minWidth: 275, maxWidth: 512 }}>
             <CardContent>
 
-                <Typography sx={{ fontWeight: 'bold' }} variant="h5" component="div" >
-                    {props.interface.base_topic.split("/").pop()}
-                </Typography>
-                <Typography sx={{ fontSize: '14px', mb: 1.5 }} color="text.secondary">
-                    {props.interface.base_topic}
-                </Typography>
+                <Box sx={{ display: 'flex', flexDirection: 'row', mb: '32px' }}>
 
-                <SliderVolts />
-                <SliderAmps />
-                <ButtonOnOff />
+                    <Box>
+
+                        <Typography sx={{ fontWeight: 'bold' }} variant="h5" component="div" >
+                            {props.interface.base_topic.split("/").pop()}
+                        </Typography>
+                        <Typography sx={{ fontSize: '14px', mb: 1.5 }} color="text.secondary">
+                            {props.interface.base_topic}
+                        </Typography>
+
+                    </Box>
+
+                    <Box sx={{ marginLeft: "auto" }}>
+
+                        <Image src="/img/power_sup.png"
+                            width={64}
+                            height={64}
+                        />
+
+                    </Box>
+
+                </Box>
+
+
+                <Box sx={{ ml: "16px", mr: "16px" }}>
+                    <SliderVolts />
+                    <SliderAmps />
+                </Box>
 
             </CardContent>
+
+
+            <CardActions
+                sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+            >
+                <ButtonOnOff />
+            </CardActions>
+
         </Card>
     );
 }
